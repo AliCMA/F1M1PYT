040f1c0992aa2fdd2370274ba983ad2026183d12
AliCMA <90691339+AliCMA@users.noreply.github.com>
2021-10-08 15:00:23 +0200

    Add files via upload

---
+++ playstatic.py
@@ -0,0 +1,19 @@
+name = "Ali_MA"
+jumpforce = 23
+power = "High JUMP"
+car = "Rollce Royce SUV"
+livesleft = just = 6 
+ammo = 250
+level = 99000
+numberoflasers = 4
+wanted = True
+
+print("name : " + str (name) + "")
+print("jumpforce : " + str (jumpforce) + "")
+print("power : " + str (power) + "")
+print("car : " + str (car) + "")
+print("livesleft : " + str (livesleft) + "")
+print("ammo : " + str (ammo) + "")
+print("level : " + str (level) +  "")
+print("numberoflasers : " + str (numberoflasers) + "")
+print("wanted : " + str (wanted) + "")
