3d7338dfb14c3f2cd56b98f55413fed40acf62f8
AliCMA <90691339+AliCMA@users.noreply.github.com>
2021-11-06 17:38:47 +0100

    Delete gedichtopdracht.py

--- gedichtopdracht.py
+++
@@ -1,7 +0,0 @@
-vers1 = "Ik ben Ali, ik kom Amtser-dam de stad van de dammen, grachten en jachten"
-vers2 = "Ik hou van basketbal en gamen met mijn vrienden, heb een hekel aan laat komen maar hou wel van bomen"
-vers3 = "In mijn vrije tijd ga ik naar buiten, ik hou niemand erbuiten of het spelen van blokfluiten"
-vers4 = "Ik kijk af en toe anime, heb een favoriete serie die vind ik heel mysteries"
-# mijn gedicht versies 
-
-print(vers1,vers2, vers3, vers4)
